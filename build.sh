c30a55ef8274022e83cceb8698c82c1b3fa5ffa8
fluff <bigfluff@disroot.org>
2021-03-21 09:35:26 -0700

    add bitlbee-facebook from github

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ apt update
 apt install -y --no-install-recommends autoconf automake build-essential gettext gcc libtool make \
 libglib2.0-dev libhttp-parser-dev libotr5-dev libpurple-dev libgnutls28-dev \
 libjson-glib-dev libpng-dev libolm-dev libprotobuf-c-dev protobuf-c-compiler \
-libgcrypt20-dev libmarkdown2-dev libpurple-dev libsqlite3-dev libwebp-dev libtool-bin
+libgcrypt20-dev libmarkdown2-dev libpurple-dev libsqlite3-dev libwebp-dev libtool-bin git
 
 cd
 curl -LO# https://get.bitlbee.org/src/bitlbee-$BITLBEE_VERSION.tar.gz
@@ -17,18 +17,27 @@ make
 make install
 make install-dev
 
+#bitlbee-facebook
+cd
+git clone https://github.com/bitlbee/bitlbee-facebook.git
+cd bitlbee-facebook
+./autogen.sh
+make
+make install
+
 # libtool --finish
 libtool --finish /usr/local/lib/bitlbee
 
 # cleanup
 apt remove -y --purge autoconf automake autotools-dev binutils binutils-common binutils-x86-64-linux-gnu build-essential \
 bzip2 cpp cpp-8 dpkg-dev g++ g++-8 gcc gcc-8 gettext gettext-base libbinutils libgcc-8-dev libsqlite3-dev libstdc++-8-dev \
-libtasn1-6-dev libtool libtool-bin m4 make nettle-dev patch xz-utils
+libtasn1-6-dev libtool libtool-bin m4 make nettle-dev patch xz-utils git
 apt clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /tmp/*
 cd
 rm -fr /root/build.sh
 rm -fr $BITLBEE_VERSION*
+rm -rf bitlbee-facebook
 
 # add user bitlbee
 adduser --system --home /var/lib/bitlbee --disabled-password --disabled-login --shell /usr/sbin/nologin bitlbee
